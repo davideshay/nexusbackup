183df5248ad98016564671c322ce8a73d25d5319
David Shay <davideshay@gmail.com>
2022-01-10 13:13:41 -0500

    add remaining backup functionality

--- app/nexusbackup.py
+++ app/nexusbackup.py
@@ -1,28 +1,23 @@
 import asyncio
 from websockets import serve
-import time, sys, os, glob
+import time, sys, os, glob, tarfile
 import requests
 from requests.auth import HTTPBasicAuth
-import time
 from datetime import datetime
 
 
 def delete_existing_backups():
-    global backup_dir
+    success=True
     files = glob.glob(backup_dir+'/*')
     for f in files:
-        print("attempting to delete file "+f,flush=True)
         try:
             os.unlink(f)
         except OSError as e:
             print("Error: %s %s" % (f,e.strerror))
-    return "Deleted files..."
+            success=False
+    return f"I {datetime.now()} Deleted existing backup files"
 
 def get_last_backup_run():
-    global nexus_svc_url
-    global task_id
-    global nexus_username
-    global nexus_password
     response = requests.get(nexus_svc_url+"/service/rest/v1/tasks/"+task_id, \
         auth = HTTPBasicAuth(nexus_username, nexus_password))
     last_run_date = None
@@ -33,23 +28,15 @@ def get_last_backup_run():
     return last_run_date
 
 def starting_backup():
-    global nexus_svc_url
-    global task_id
-    global nexus_username
-    global nexus_password
     response = requests.post(nexus_svc_url+"/service/rest/v1/tasks/" + task_id + "/run", \
        auth = HTTPBasicAuth(nexus_username, nexus_password))
     if response.status_code in (200,204):
-        reply="Backup Task Successfully started..."
+        reply=f"I {datetime.now()} Backup Task Successfully started..."
     else:
-        reply="Error starting task, status was "+str(response.status_code)
+        reply=f"E {datetime.now()} Error starting task, status was {response.status_code}"
     return reply
 
 def waiting_backup(initial_last_run_date):
-    global nexus_svc_url
-    global task_id
-    global nexus_username
-    global nexus_password
     task_finished=False
     current_last_run_date=None
     while not task_finished:
@@ -62,57 +49,102 @@ def waiting_backup(initial_last_run_date):
                 if  current_last_run_date > initial_last_run_date:
                     task_finished=True
         time.sleep(2)
-    reply="Backup completed on "+str(current_last_run_date)
+    reply=f"I {datetime.now()} Backup completed"
     return reply
 
-def bundle_files():
-    return "Bundling files"
+def tar_backup_files(backup_file_name):
+    success=True
+    with tarfile.open(backup_file_name,"a:") as tar:
+        files = glob.glob(backup_dir+'/*.bak')
+        for f in files:
+            try:
+                print("backing up file "+str(f))
+                tar.add(f)
+            except OSError as e:
+                print("Error: %s %s" % (f,e.strerror))
+                success=False
+        tar.close()
+    if success:
+        reply=f"I {datetime.now()} Backup bundled/tar created"
+    else:
+        reply=f"E {datetime.now()} Error creating bundle/tar"
+    return reply
 
-def rsync_files():
-    return "Copying backup files"
+def tar_blob_files(backup_file_name):
+    success=True
+    with tarfile.open(backup_file_name,"a:") as tar:
+        try:
+            tar.add(f"{blob_dir}/")
+        except OSError as e:
+            print(f"Error {f} {e.strerror}")
+            success=False
+        tar.close()
+    if success:
+        reply=f"I {datetime.now()} Blob bundled/tar appended"
+    else:
+        reply=f"E {datetime.now()} Error appending blob bundle"
+    return reply
 
-def remove_files():
-    return "Cleaning up"
-
+def tar_keystore_files(backup_file_name):
+    success=True
+    with tarfile.open(backup_file_name,"a:") as tar:
+        try:
+            tar.add(f"{id_dir}/")
+        except OSError as e:
+            print(f"Error {f} {e.strerror}")
+            success=False
+        tar.close()
+    if success:
+        reply=f"I {datetime.now()} Keystore node/id bundled/tar appended"
+    else:
+        reply=f"E {datetime.now()} Error appending keystore node/id"
+    return reply
 
 async def backup(websocket):
     async for message in websocket:
         if message=="start":
+            backup_run_time=datetime.now().strftime('%Y%m%d-%H%M')
+            backup_file_name=f"{target_dir}/nxsbackup-{backup_run_time}.tar"
+            print(f"I {datetime.now()} Backup process initiated...")
             backupdetails=delete_existing_backups()
             await websocket.send(backupdetails)
             initial_last_run_date=get_last_backup_run()
-            backupdetails="Last run  of backup was "+str(initial_last_run_date)
+            backupdetails=f"I {datetime.now()} Last run  of backup was {initial_last_run_date}"
             await websocket.send(backupdetails)
             backupdetails=starting_backup()
             await websocket.send(backupdetails)
             backupdetails=waiting_backup(initial_last_run_date)
             await websocket.send(backupdetails)
-            backupdetails=bundle_files()
+            backupdetails=tar_backup_files(backup_file_name)
             await websocket.send(backupdetails)
-            backupdetails=rsync_files()
+            backupdetails=tar_blob_files(backup_file_name)
             await websocket.send(backupdetails)
-            backupdetails=remove_files()
+            backupdetails=tar_keystore_files(backup_file_name)
+            await websocket.send(backupdetails)
+            backupdetails=delete_existing_backups()
             await websocket.send(backupdetails)
             backupdetails="end"
             await websocket.send(backupdetails)
-
+            print(f"I {datetime.now()} Backup process completed...")
 
 async def main():
-    async with serve(backup, "0.0.0.0", 8000):
+    async with serve(backup, "0.0.0.0", 8000,ping_timeout=None,close_timeout=None):
         await asyncio.Future()  # run forever
 
 
 backup_dir=os.environ.get('BACKUPDIR')
+nexus_base_dir=os.environ.get('NEXUSBASEDIR')
+target_dir=os.environ.get('TARGETDIR')
 task_id=os.environ.get('TASKID')
 nexus_username=os.environ.get('NEXUS_USERNAME')
 nexus_password=os.environ.get('NEXUS_PASSWORD')
 nexus_svc_hostname=os.environ.get('NEXUS_SVC_HOSTNAME')
 nexus_svc_port=os.environ.get('NEXUS_SVC_PORT')
-print("hostname is "+nexus_svc_hostname)
-print("port is "+nexus_svc_port)
-
 nexus_svc_url="http://"+nexus_svc_hostname+":"+nexus_svc_port
-print("url is "+nexus_svc_url)
+blob_dir=f"{nexus_base_dir}/blobs"
+id_dir=f"{nexus_base_dir}/keystores/node"
+
+
 
 
 asyncio.run(main())
