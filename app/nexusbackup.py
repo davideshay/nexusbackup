d4a67231726f60aaf734800a22b2e923468b3f27
David Shay <davideshay@gmail.com>
2022-01-09 23:58:51 -0500

    changed backup logic

--- app/nexusbackup.py
+++ app/nexusbackup.py
@@ -3,10 +3,12 @@ from websockets import serve
 import time, sys, os, glob
 import requests
 from requests.auth import HTTPBasicAuth
-import time, ciso8601
+import time
+from datetime import datetime
 
 
 def delete_existing_backups():
+    global backup_dir
     files = glob.glob(backup_dir+'/*')
     for f in files:
         print("attempting to delete file "+f,flush=True)
@@ -16,21 +18,25 @@ def delete_existing_backups():
             print("Error: %s %s" % (f,e.strerror))
     return "Deleted files..."
 
-def get_last_backup_run()
+def get_last_backup_run():
+    global nexus_svc_url
+    global task_id
+    global nexus_username
+    global nexus_password
     response = requests.get(nexus_svc_url+"/service/rest/v1/tasks/"+task_id, \
         auth = HTTPBasicAuth(nexus_username, nexus_password))
-    if response.status_code in 200:
-        print("initial get response content is:",flush=True)
+    last_run_date = None
+    if response.status_code == 200:
         answer=response.json()
-        print(answer)
-        last_run=answer["lastRun"]
-
-        print("answer.lastRun " + answer["lastRun"])
-
-
-
+        last_run_str=answer["lastRun"]
+        last_run_date=datetime.fromisoformat(last_run_str)
+    return last_run_date
 
 def starting_backup():
+    global nexus_svc_url
+    global task_id
+    global nexus_username
+    global nexus_password
     response = requests.post(nexus_svc_url+"/service/rest/v1/tasks/" + task_id + "/run", \
        auth = HTTPBasicAuth(nexus_username, nexus_password))
     if response.status_code in (200,204):
@@ -39,23 +45,25 @@ def starting_backup():
         reply="Error starting task, status was "+str(response.status_code)
     return reply
 
-def waiting_backup():
+def waiting_backup(initial_last_run_date):
+    global nexus_svc_url
+    global task_id
+    global nexus_username
+    global nexus_password
     task_finished=False
+    current_last_run_date=None
     while not task_finished:
         response = requests.get(nexus_svc_url+"/service/rest/v1/tasks/"+task_id, \
             auth = HTTPBasicAuth(nexus_username, nexus_password))
-        print("response content is:",flush=True)
-        answer=response.json()
-        print(answer)
-        print("answer.currentState " + answer["currentState"])
-
+        if response.status_code == 200:
+            answer=response.json()
+            if answer["currentState"]=="WAITING":
+                current_last_run_date=datetime.fromisoformat(answer["lastRun"])
+                if  current_last_run_date > initial_last_run_date:
+                    task_finished=True
         time.sleep(2)
-
-
-
-
-
-    return "Waiting for backup to complete"
+    reply="Backup completed on "+str(current_last_run_date)
+    return reply
 
 def bundle_files():
     return "Bundling files"
@@ -72,9 +80,12 @@ async def backup(websocket):
         if message=="start":
             backupdetails=delete_existing_backups()
             await websocket.send(backupdetails)
+            initial_last_run_date=get_last_backup_run()
+            backupdetails="Last run  of backup was "+str(initial_last_run_date)
+            await websocket.send(backupdetails)
             backupdetails=starting_backup()
             await websocket.send(backupdetails)
-            backupdetails=waiting_backup()
+            backupdetails=waiting_backup(initial_last_run_date)
             await websocket.send(backupdetails)
             backupdetails=bundle_files()
             await websocket.send(backupdetails)
@@ -86,8 +97,6 @@ async def backup(websocket):
             await websocket.send(backupdetails)
 
 
-
-
 async def main():
     async with serve(backup, "0.0.0.0", 8000):
         await asyncio.Future()  # run forever
@@ -97,8 +106,13 @@ backup_dir=os.environ.get('BACKUPDIR')
 task_id=os.environ.get('TASKID')
 nexus_username=os.environ.get('NEXUS_USERNAME')
 nexus_password=os.environ.get('NEXUS_PASSWORD')
-nexus_svc_hostname=os.environ.get('NEXUS_SERVICE_HOST')
-nexus_svc_port=os.environ.get('NEXUS_SERVICE_PORT_WEB')
+nexus_svc_hostname=os.environ.get('NEXUS_SVC_HOSTNAME')
+nexus_svc_port=os.environ.get('NEXUS_SVC_PORT')
+print("hostname is "+nexus_svc_hostname)
+print("port is "+nexus_svc_port)
+
 nexus_svc_url="http://"+nexus_svc_hostname+":"+nexus_svc_port
+print("url is "+nexus_svc_url)
+
 
 asyncio.run(main())
